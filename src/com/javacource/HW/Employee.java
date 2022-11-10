2dbbb3649b9c02eb8d9bf0d0352a76b6cf5a9c9b
user <yulechkaakopyan@yandex.ru>
2022-11-10 17:23:25 +0300

    Курсовая работа

---
+++ src/com/javacource/HW/Employee.java
@@ -0,0 +1,49 @@
+package com.javacource.HW;
+import java.util.Objects;
+
+public class Employee {
+    private final String name;
+    private static int counter = 0;
+    public int id;
+    private int salary;
+    private int department;
+
+    public Employee(String name, int salary, int department) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+        this.id = counter++;
+
+    }
+
+    public int getId() {return}
+
+    public String getName {return this.name;}
+
+    public int getSalary() {return this.salary;}
+
+    public void setSalary(int Salary) {this.salary = salary;}
+
+    public int getDepartment() {return this.department;}
+
+    public void setDepartment(int department) {this.department = department;}
+
+    public String toString() {
+        return
+                "id: " + id +
+                     ";Сотрудник: " + name +
+                     ";Зарплата: " + salary +
+                     ";Отдел: " + department +
+                     ";";
+    }
+    public boolean equels (Objects o) {
+        if (this == 0) return true;
+        if (0 == null || getClass() !=o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && salary == employee.salary && department == employee.department &&
+        Objects.equals(name,employee.name);
+    }
+    public int hashCode () {
+        return Objects.hash(name,id,salary,department);
+    }
+}
---
+++ src/com/javacource/HW/com/Main.java
@@ -0,0 +1,63 @@
+package com.javacource.HW.com;
+
+import com.javacource.HW.Employee;
+
+public class Main {
+    private static Employee[] employees = new Employee[10];
+
+    public static void printEmployees() {
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+
+    public static int calculateTotalSalary() {
+        int sum = 0;
+        for (Employee employee : employees) {
+            sum += employee.getSalary();
+        }
+        return sum;
+    }
+
+    public static Employee findEmployeeWithMinSalary() {
+        Employee result = employees[0];
+        int minSalary = employees[0].getSalary();
+        for (Employee employee : employees) {
+            if (employee.getSalary() < minSalary) {
+                minSalary = employee.getSalary();
+                result = employee;
+            }
+        }
+        return result;
+    }
+
+    public static float calculateAverageSalary() {
+        return calculateTotalSalary() / (float) employees.length;
+    }
+
+    public static void ppintFullName() {
+        for (Employee employee : employees) {
+        }
+    }
+
+    public static void main(String[] args) {
+        employees = new Employee[]{
+                new Employee("Гаримов Андрей Юрьевич", 12000, 1),
+                null,
+                new Employee("Сидоренко Виталий Максимович", 50000, 2),
+                new Employee("Козлова Татьяна Ивановна", 7500, 2),
+                new Employee("Коч Елена Игоревна", 22000, 2),
+                new Employee("Мелик Игорь Игоревич", 18000, 3),
+                new Employee("Ромина Галина Юрьевна", 19000, 1),
+                new Employee("Гурин Андрей Владиславович", 300_000, 3),
+                new Employee("Сыч Даниил Артемович", 200_000, 3),
+                new Employee("Кузин Евгений Олегович", 150_000, 2)
+        };
+        employees[0] = new Employee("Гаримов Андрей Юрьевич", 12000, 1);
+        ppintFullName();
+        printEmployees();
+        System.out.println("avg =" + calculateAverageSalary());
+        System.out.println("  high  ");
+
+    }
+}
