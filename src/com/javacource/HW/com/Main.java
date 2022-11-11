dd642e10e0f869125e3e233cbc179a37ae44d368
YULIYA <114095662+Yuliya1201@users.noreply.github.com>
2022-11-11 16:15:48 +0300

    Update Main.java
    Оставила в Main только main

--- src/com/javacource/HW/com/Main.java
+++ src/com/javacource/HW/com/Main.java
@@ -3,6 +3,25 @@ package com.javacource.HW.com;
 import com.javacource.HW.Employee;
 
 public class Main {
+    
+      public static void main(String[] args) {
+        employees = new Employee[]{
+                new Employee("Гаримов Андрей Юрьевич", 12000, 1),
+                null,
+                new Employee("Сидоренко Виталий Максимович", 50000, 2),
+                new Employee("Козлова Татьяна Ивановна", 7500, 2),
+                new Employee("Коч Елена Игоревна", 22000, 2),
+                new Employee("Мелик Игорь Игоревич", 18000, 3),
+                new Employee("Ромина Галина Юрьевна", 19000, 1),
+                new Employee("Гурин Андрей Владиславович", 300_000, 3),
+                new Employee("Сыч Даниил Артемович", 200_000, 3),
+                new Employee("Кузин Евгений Олегович", 150_000, 2)
+        };
+        employees[0] = new Employee("Гаримов Андрей Юрьевич", 12000, 1);
+        ppintFullName();
+        printEmployees();
+        System.out.println("avg =" + calculateAverageSalary());
+        System.out.println("  high  ");
     private static Employee[] employees = new Employee[10];
 
     public static void printEmployees() {
@@ -40,24 +59,3 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
-        employees = new Employee[]{
-                new Employee("Гаримов Андрей Юрьевич", 12000, 1),
-                null,
-                new Employee("Сидоренко Виталий Максимович", 50000, 2),
-                new Employee("Козлова Татьяна Ивановна", 7500, 2),
-                new Employee("Коч Елена Игоревна", 22000, 2),
-                new Employee("Мелик Игорь Игоревич", 18000, 3),
-                new Employee("Ромина Галина Юрьевна", 19000, 1),
-                new Employee("Гурин Андрей Владиславович", 300_000, 3),
-                new Employee("Сыч Даниил Артемович", 200_000, 3),
-                new Employee("Кузин Евгений Олегович", 150_000, 2)
-        };
-        employees[0] = new Employee("Гаримов Андрей Юрьевич", 12000, 1);
-        ppintFullName();
-        printEmployees();
-        System.out.println("avg =" + calculateAverageSalary());
-        System.out.println("  high  ");
-
-    }
-}
